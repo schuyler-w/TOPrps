fa09cf8108bb6b54f9205d50b852b882be185f20
Schuyler Wang <schuyler.rw@gmail.com>
2025-07-13 19:36:06 -0700

    Added UI functionality

--- rps.js
+++ rps.js
@@ -5,51 +5,70 @@
 var humanScore = 0;
 var computerScore = 0;
 
+const rockbtn = document.createElement("button");
+const paperbtn = document.createElement("button");
+const scissorsbtn = document.createElement("button");
+
+rockbtn.addEventListener("click", () => play(0));
+paperbtn.addEventListener("click", () => play(1));
+scissorsbtn.addEventListener("click", () => play(2));
+
+rockbtn.textContent = "ROCK";
+paperbtn.textContent = "PAPER";
+scissorsbtn.textContent = "SCISSORS";
+
+const scoreDiv = document.createElement("div");
+
+const header = document.createElement("h1");
+header.textContent = "Score Board:"
+const score = document.createElement("p");
+score.textContent = `Your score: ${humanScore}, CPU score: ${computerScore}.`;
+
+let result = document.createElement("p");
+result.textContent = "Click a button to start!";
+
+scoreDiv.appendChild(header);
+scoreDiv.appendChild(score);
+scoreDiv.appendChild(result);
+
+const body = document.querySelector("body");
+body.appendChild(scoreDiv);
+body.appendChild(rockbtn);
+body.appendChild(paperbtn);
+body.appendChild(scissorsbtn);
+
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
-function getHumanChoice() {
-    let choice = prompt("Choice: ").toLowerCase()
-    let intChoice;
-    if (choice === "rock"){
-        intChoice = 0;
-    } else if (choice === "paper") {
-        intChoice = 1;
-    } else if (choice === "scissors") {
-        intChoice = 2;
-    }
-
-    return intChoice
-}
-
-function play() {
-    let human = getHumanChoice();
+function play(selection) {
+    let human = selection;
     let cpu = getComputerChoice();
+
+    if (human === cpu) {
+        result.textContent = "TIE";
+    }
 
     if (human > cpu) {
         if (human === 2 && cpu === 0) {
             computerScore++;
+            result.textContent = "YOU LOSE";
         }
         else {
             humanScore++;
+            result.textContent = "YOU WIN";
         }
     } else if (cpu > human) {
         if (cpu === 2 && human === 0) {
             humanScore++;
+            result.textContent = "YOU WIN";
         }
         else {
             computerScore++;
+            result.textContent = "YOU LOSE"
         }
     }
+    score.textContent = `Your score: ${humanScore}, CPU score: ${computerScore}.`;
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        play();
-    }
-    console.log(`Final Score: You: ${humanScore}, Computer: ${computerScore}`)
-}
-
-playGame();
 
